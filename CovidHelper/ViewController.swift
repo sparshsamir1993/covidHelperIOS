e07a11c92abc02e482940f981e2d6210cc66f6e3
sparshsamir <sparshsamir1993@gmail.com>
2020-06-09 17:12:51 -0400

    Initial Commit

---
+++ CovidHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidHelper
+//
+//  Created by Sparsh Samir on 2020-06-09.
+//  Copyright © 2020 Sparsh Samir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
