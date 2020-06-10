030bfe178f9d3ddcece0a8d1ca3cdefb7923b14f
sparshsamir <sparshsamir1993@gmail.com>
2020-06-09 20:35:42 -0400

    2 views in tabs created and filled with content

---
+++ CovidHelper/QuestionnaireViewController.swift
@@ -0,0 +1,26 @@
+//
+//  QuestionnaireViewController.swift
+//  CovidHelper
+//
+//  Created by Sparsh Samir on 2020-06-09.
+//  Copyright © 2020 Sparsh Samir. All rights reserved.
+//
+
+
+import UIKit
+
+class QuestionnaireViewController: UIViewController {
+    @IBOutlet var answerOne: UISwitch!
+    @IBOutlet var answerTwo: UISwitch!
+    @IBOutlet var answerThree: UISwitch!
+    @IBOutlet var answerFour: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+            
+    }
+
+    
+}
--- CovidHelper/ViewController.swift
+++ CovidHelper/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var nameField: UITextField!
+    @IBOutlet var dobPicker: UIDatePicker!
+    @IBAction func getUserNameField(_ sender: UITextField) {
+        
+//        nameField.text
+    }
+    @IBAction func dobSelected(_ sender: UIDatePicker) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        dobPicker.datePickerMode = UIDatePicker.Mode.date
     }
-
-
+    
 }
 
