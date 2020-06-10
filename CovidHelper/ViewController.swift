7f5c03b7f9b45304e215aa7848403f1023cde83a
sparshsamir <sparshsamir1993@gmail.com>
2020-06-10 00:49:22 -0400

    userInfo functionality done

--- CovidHelper/ViewController.swift
+++ CovidHelper/ViewController.swift
@@ -12,18 +12,44 @@ class ViewController: UIViewController {
     
     @IBOutlet var nameField: UITextField!
     @IBOutlet var dobPicker: UIDatePicker!
+    @IBOutlet var userSubmitButton: UIButton!
+    @IBOutlet var errorLabel: UILabel!
     @IBAction func getUserNameField(_ sender: UITextField) {
         
 //        nameField.text
+        errorLabel.text=""
     }
     @IBAction func dobSelected(_ sender: UIDatePicker) {
     }
     
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        nameField.resignFirstResponder()
+    }
+    
+    @IBAction func infoSubmit(_ sender: UIButton) {
+//        let userDOB = dobPicker.date
+        let userName:String? = nameField.text
+        if userName != "" && userName != nil{
+            if  let arrayOfTabBarItems = tabBarController?.tabBar.items as AnyObject as? NSArray,let tabBarItem = arrayOfTabBarItems[1] as? UITabBarItem {
+                tabBarItem.isEnabled = true
+            }
+            
+        }else{
+            errorLabel.text = "Please provide your name"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        errorLabel.text = ""
         dobPicker.datePickerMode = UIDatePicker.Mode.date
+        dobPicker.maximumDate = Date()
+        userSubmitButton.layer.cornerRadius = 7
+        if  let arrayOfTabBarItems = tabBarController?.tabBar.items as AnyObject as? NSArray,let tabBarItem = arrayOfTabBarItems[1] as? UITabBarItem {
+            tabBarItem.isEnabled = false
+        }
+        
     }
     
 }
